e30b6ced3df4ae90873e0529b75b3234a0eb391b
Jake Goldfarb <jgoldfarb@composecure.com>
2024-12-11 17:31:59 -0500

    feature/Add and start work on Home/dice state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import DicePropForm from "@/components/dice-prop-form";
+import { useState } from "react";
+
 export default function Home() {
+	const diceDefaults = {
+		sides: 4,
+		material: "material1",
+		rigidness: 50,
+	};
+
+	const [sides, setSides] = useState(diceDefaults.sides);
+	const [material, setMaterial] = useState(diceDefaults.material);
+	const [rigidness, setRigidness] = useState(diceDefaults.rigidness);
+
+	const setDiceProp = {
+		sides: setSides,
+		material: setMaterial,
+		rigidness: setRigidness,
+	};
+
+	const handleDicePropChange = (key: string, value: unknown) =>
+		console.table({ sides, material, rigidness });
+
 	return (
-		<div>
-			<main></main>
+		<div id="root">
+			<main className="flex-1 flex items-center justify-center">
+				<DicePropForm
+					onDicePropsChange={handleDicePropChange}
+					diceDefaults={diceDefaults}
+					diceProps={{ sides, material, rigidness }}
+				/>
+			</main>
 			<footer></footer>
 		</div>
 	);
