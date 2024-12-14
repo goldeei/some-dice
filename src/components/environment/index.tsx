d649f9ff3cc4253e08cb56d956a08e10b394c3ea
Jake Goldfarb <jake.goldiee@gmail.com>
2024-12-13 23:53:53 -0500

    Add sides

--- src/components/environment/index.tsx
+++ src/components/environment/index.tsx
@@ -1,5 +1,15 @@
+import { RigidBody } from "@react-three/rapier";
+
 import { Side } from "./side";
 
 export const Environment = ({}) => {
-	return <Side />;
+	return (
+		<RigidBody type="fixed">
+			<Side isFloor />
+			<Side zSide="back" xSide="left" />
+			<Side zSide="back" xSide="right" />
+			<Side zSide="front" xSide="right" />
+			<Side zSide="front" xSide="left" />
+		</RigidBody>
+	);
 };
