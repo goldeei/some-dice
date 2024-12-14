c523a7d4c757cc649ac6dc63c8c3cbe09f0860e0
Jake Goldfarb <jake.goldiee@gmail.com>
2024-12-13 23:53:02 -0500

    Rename rot to rotArr

--- src/lib/3d-helpers.ts
+++ src/lib/3d-helpers.ts
@@ -28,22 +28,19 @@ export const getRotationFromDegrees = (deg: RotationMinMax) => {
  *   and applying a conversion would be redundant.
  * - If no rotation is provided, we fall back to `[0, 0, 0]`, which represents no rotation.
  *
- * @param rot - The array of rotation values in degrees, where each element corresponds to an axis (x, y, z). Positive 0-360.
+ * @param rotArr - The array of rotation values in degrees, where each element corresponds to an axis (x, y, z). Positive 0-360.
  * @returns A new array with rotation values in radians.
  */
-export const getRotationArrayFromDegrees = (rot: RotationArray) => {
+export const getRotationArrayFromDegrees = (rotArr: RotationArray) => {
 	// Default to [0, 0, 0] if no rotation is provided
-	if (rot.every((num) => num === 0)) return rot;
+	if (rotArr.every((num) => num === 0)) return rotArr;
 
 	// Convert each non-zero rotation value to radians, leaving zero values unchanged
 	// Zero values are retained because they represent no rotation and don't require modification.
-	const rotation = rot.map((deg) =>
+	const rotation = rotArr.map((deg) =>
 		deg !== 0 ? getRotationFromDegrees(deg) : deg
 	) as RotationArray;
 
 	// Return the potentially modified rotation array with values in radians
 	return rotation;
 };
-
-// POSITION
-// export const
