f420a51f21323d4e2bd14459b46e3c14bdb8eede
Jake Goldfarb <jake.goldiee@gmail.com>
2024-12-15 20:52:35 -0500

    Map dice with random impulses

--- src/components/dice/index.tsx
+++ src/components/dice/index.tsx
@@ -1,5 +1,71 @@
+import { RapierRigidBody } from "@react-three/rapier";
+import { useRef, useState } from "react";
+
 import { Die } from "./die";
 
+const rollXMin = -0.1;
+const rollXMax = 0.1;
+
+const rollYMin = -0.05;
+const rollYMax = 0.01;
+
+const rollZMin = -0.05;
+const rollZMax = -0.1;
+
 export const Dice = ({}) => {
-	return <Die />;
+	const dice = ["dice1", "dice2", "dice3", "dice4", "dice5"];
+	const colors = ["red", "blue", "purple", "yellow", "orange"];
+	const diceRefs = useRef<(RapierRigidBody | null)[]>([]);
+
+	const [gravityScale, setGravityScale] = useState(0);
+
+	const getOffsetPosition = (i: number) => {
+		let x: number;
+		const arrayMiddle = Math.floor(dice.length / 2);
+		if (i === arrayMiddle) {
+			x = 0;
+		} else {
+			x = 0.5 * (i - arrayMiddle);
+		}
+		return [x, 0, 0];
+	};
+
+	const handleRoll = () => {
+		if (diceRefs.current) {
+			setGravityScale(1);
+			diceRefs.current.forEach((dieRef) => {
+				console.log({
+					x: Math.random() * (rollXMax - rollXMin) + rollXMin,
+					y: Math.random() * (rollYMax - rollYMin) + rollYMin,
+					z: Math.random() * (rollZMax - rollZMin) + rollZMin,
+				});
+				if (dieRef) {
+					dieRef.applyImpulse(
+						{
+							x: Math.random() * (rollXMax - rollXMin) + rollXMin,
+							y: Math.random() * (rollYMax - rollYMin) + rollYMin,
+							z: Math.random() * (rollZMax - rollZMin) + rollZMin,
+						},
+						true
+					);
+				}
+			});
+		}
+	};
+
+	return (
+		<group name="dice" onClick={handleRoll}>
+			{dice.map((_, i) => (
+				<Die
+					key={i}
+					ref={(die: RapierRigidBody | null) => {
+						diceRefs.current[i] = die;
+					}}
+					gravityScale={gravityScale}
+					position={getOffsetPosition(i)}
+					color={colors[i]}
+				/>
+			))}
+		</group>
+	);
 };
