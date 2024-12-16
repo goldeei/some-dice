62515c375884d0f7a196aad02061dcafcdee5dc9
Jake Goldfarb <jake.goldiee@gmail.com>
2024-12-15 21:00:52 -0500

    Add impulse min and max values

--- src/app/constants/dice.ts
+++ src/app/constants/dice.ts
@@ -1,11 +1,26 @@
+// DICE PROPS
 export const MATERIALS = ["material1", "material2", "material3"];
-
 export const SIDE_MINMAX = {
 	min: 2,
 	max: 20,
 };
-
 export const RIGIDNESS_MINMAX = {
 	min: 0,
 	max: 100,
+};
+
+// ROLL
+export const ROLL_IMPULSE_MINMAX = {
+	x: {
+		min: -0.1,
+		max: 0.1,
+	},
+	y: {
+		min: -0.05,
+		max: 0.01,
+	},
+	z: {
+		min: -0.05,
+		max: -0.1,
+	},
 };
