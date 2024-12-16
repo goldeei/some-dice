551fd008e884e4ed10bb7499db837cc1b7b6c76b
Jake Goldfarb <jake.goldiee@gmail.com>
2024-12-15 21:01:20 -0500

    Add randomBetweenOneAndZero

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+import type { ClassValue } from "clsx";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const randomBetweenOneAndZero = (min: number, max: number) =>
+	Math.random() * (max - min) + min;
