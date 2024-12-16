de5d782d312d128ca7841831025a669068eadaa4
Jake Goldfarb <jake.goldiee@gmail.com>
2024-12-15 19:00:37 -0500

    Add position array type

--- src/types/3d.ts
+++ src/types/3d.ts
@@ -3,3 +3,6 @@ import { IntRange } from "./utils";
 // ROTATION
 export type RotationMinMax = IntRange<0, 360>;
 export type RotationArray = [RotationMinMax, RotationMinMax, RotationMinMax];
+
+// POSITION
+export type PositionArray = [number, number, number];
