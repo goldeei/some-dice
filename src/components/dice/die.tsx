23ec73d80bb11f90c40b3fcc6f80d98b6d1b842b
Jake Goldfarb <jake.goldiee@gmail.com>
2024-12-15 19:00:51 -0500

    Add die component

--- src/components/dice/die.tsx
+++ src/components/dice/die.tsx
@@ -1,26 +1,25 @@
 import { RoundedBox } from "@react-three/drei";
-import { RapierRigidBody, RigidBody } from "@react-three/rapier";
-import { useRef, useState } from "react";
+import {
+	RapierRigidBody,
+	RigidBody,
+	RigidBodyProps,
+} from "@react-three/rapier";
 
-interface Die {
+type Die = RigidBodyProps & {
+	ref: (die: RapierRigidBody | null) => void;
 	color?: string;
-}
+};
 export const Die = ({ ...props }: Die) => {
-	const { color = "lightgrey" } = props;
-
-	const dieRef = useRef<RapierRigidBody>(null);
-	const [gravityScale, setGravityScale] = useState(0);
-
-	const handleRoll = () => {
-		if (dieRef.current) {
-			setGravityScale(1);
-			dieRef.current.applyImpulse({ x: 0, y: -0.05, z: -0.1 }, true);
-		}
-	};
+	const { color = "lightgrey", ref, gravityScale, position } = props;
 
 	return (
-		<RigidBody ref={dieRef} gravityScale={gravityScale} density={1.5}>
-			<RoundedBox args={[0.25, 0.25, 0.25]} onClick={handleRoll}>
+		<RigidBody
+			ref={ref}
+			density={1.5}
+			gravityScale={gravityScale}
+			position={position}
+		>
+			<RoundedBox args={[0.25, 0.25, 0.25]} position={position}>
 				<meshStandardMaterial color={color} />
 			</RoundedBox>
 		</RigidBody>
